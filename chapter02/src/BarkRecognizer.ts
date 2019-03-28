340c6d20d8b9c1b6fce613b74c45aa383a4f4640
Yunseop Kim <undefined@mealant.com>
2019-03-28 20:40:14 +0900

    delegate

--- chapter02/src/BarkRecognizer.ts
+++ chapter02/src/BarkRecognizer.ts
@@ -1,12 +1,17 @@
 import { DogDoor } from "./DogDoor";
+import { Bark } from "./Bark";
 export type BarkRecognizer = InstanceType<typeof BarkRecognizer>;
 export const BarkRecognizer = class {
   private _door: DogDoor;
   constructor(dogDoor: DogDoor) {
     this._door = dogDoor;
   }
-  public recognize(bark: string): void {
-    console.log(`BarkRecognizer: Heard a ${bark}`);
-    this._door.open();
+  public recognize(bark: Bark): void {
+    console.log(`BarkRecognizer: Heard a ${bark.sound}`);
+    if (this._door.allowedBark.equals(bark)) {
+      this._door.open();
+    } else {
+      console.log("this dog is not allowed.")
+    }
   }
 };
